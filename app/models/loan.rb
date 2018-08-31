ca98cf3d9f0f6b67d2c53ef54d92324c8f373204
z <x@y.com>
2018-08-31 15:03:30 -0700

    Errors appearing mostly successfully

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -3,4 +3,13 @@ class Loan < ApplicationRecord
   validates :mailing_address, presence: { message: "Please fill in the form completely" }
   validates :annual_income, presence: { message: "Please fill in the form completely" }
   validates :requested_loan_amount, presence: { message: "Please fill in the form completely" }
+  before_validation :do_the_math
+
+  def do_the_math
+    req = attributes["requested_loan_amount"].to_i.to_f
+    inc = attributes["annual_income"].to_i.to_f
+    if req > 50000.0 || req < 5000.0 || (req / inc) * 100.0 > 30.0
+      self.errors.add :base, "Loan application unsuccessful"
+    end
+  end
 end
