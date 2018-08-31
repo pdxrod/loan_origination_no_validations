bc5af9b795bc48556127b68738b293567faede78
z <x@y.com>
2018-08-31 14:24:45 -0700

    More tests, covering all the requirements - when they pass, the project is done

--- spec/features/loans_feature_spec.rb
+++ spec/features/loans_feature_spec.rb
@@ -6,9 +6,32 @@ RSpec.describe LoansController, type: :feature do
     Loan.delete_all
   end
 
-  describe "testing data" do
-    it "should have the right data for testing" do
-    end
+  def fill_in_with_too_low_loan_amount
+    fill_in "loan_name", with: "Rod McLaughlin"
+    fill_in "loan_mailing_address", with: "16409 Division"
+    fill_in "loan_annual_income", with:       "200000"
+    fill_in "loan_requested_loan_amount", with: "4999"
+  end
+
+  def fill_in_with_absolutely_too_high_loan_amount
+    fill_in "loan_name", with: "Rod McLaughlin"
+    fill_in "loan_mailing_address", with: "16409 Division"
+    fill_in "loan_annual_income", with:        "200000"
+    fill_in "loan_requested_loan_amount", with: "50001"
+  end
+
+  def fill_in_with_too_high_loan_amount_for_income
+    fill_in "loan_name", with: "Rod McLaughlin"
+    fill_in "loan_mailing_address", with: "16409 Division"
+    fill_in "loan_annual_income", with:         "50000"
+    fill_in "loan_requested_loan_amount", with: "15001"
+  end
+
+  def fill_in_with_adequate_income_and_loan_amount
+    fill_in "loan_name", with: "Rod McLaughlin"
+    fill_in "loan_mailing_address", with: "16409 Division"
+    fill_in "loan_annual_income", with:         "50000"
+    fill_in "loan_requested_loan_amount", with: "15000"
   end
 
   describe "visiting loan page" do
@@ -17,23 +40,65 @@ RSpec.describe LoansController, type: :feature do
       expect(page).to have_content "Name"
     end
 
-    it "should show an error if you don't enter anything and hit submit" do
+    it "should show 'please fill in' if you don't enter anything and hit submit" do
       visit '/'
       click_on "submit"
       expect(page).to have_content "Please fill in the form completely"
       expect(page).not_to have_content "Loan application successful"
+      expect(page).not_to have_content "Loan application unsuccessful"
     end
 
-    it "should not show the 'please fill in' error if you fill in the form and hit submit" do
+    it "should show 'successful' if you fill in the form with adequate information and hit submit" do
       visit '/'
-      fill_in "loan_name", with: "Rod McLaughlin"
-      fill_in "loan_mailing_address", with: "16409 Division"
-      fill_in "loan_annual_income", with: "1"
-      fill_in "loan_requested_loan_amount", with: "1000000"
+      fill_in_with_adequate_income_and_loan_amount
       click_on "submit"
       expect(page).not_to have_content "Please fill in the form completely"
+      expect(page).not_to have_content "Loan application unsuccessful"
       expect(page).to have_content "Loan application successful"
     end
-  end
 
+    it "should show 'unsuccessful' if you enter too low loan amount and hit submit" do
+      visit '/'
+      fill_in_with_too_low_loan_amount
+      click_on "submit"
+      expect(page).not_to have_content "Please fill in the form completely"
+      expect(page).to have_content "Loan application unsuccessful"
+      expect(page).not_to have_content "Loan application successful"
+    end
+
+    it "should show 'unsuccessful' if you enter absolutely too high loan amount and hit submit" do
+      visit '/'
+      fill_in_with_absolutely_too_high_loan_amount
+      click_on "submit"
+      expect(page).not_to have_content "Please fill in the form completely"
+      expect(page).to have_content "Loan application unsuccessful"
+      expect(page).not_to have_content "Loan application successful"
+    end
+
+    it "should show 'unsuccessful' if you enter too high loan amount for your income and hit submit" do
+      visit '/'
+      fill_in_with_too_high_loan_amount_for_income
+      click_on "submit"
+      expect(page).not_to have_content "Please fill in the form completely"
+      expect(page).to have_content "Loan application unsuccessful"
+      expect(page).not_to have_content "Loan application successful"
+    end
+
+    it "should save the application if you fill in the form with adequate information and hit submit" do
+      n = Loan.count
+      visit '/'
+      fill_in_with_adequate_income_and_loan_amount
+      click_on "submit"
+      expect(Loan.count).to eq n + 1
+    end
+
+    it "should always persist the data if you hit submit, even if you don't fill it in correctly" do
+      n = Loan.count
+      visit '/'
+      fill_in "loan_name", with: "Rod McLaughlin"
+      click_on "submit"
+      expect(Loan.count).to eq n + 1
+    end
+
+  end
 end
